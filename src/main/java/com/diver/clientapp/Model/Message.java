0d9b52d5dea4bd08f1d95d83dc12c469778f0424
campo22 <miguelcampodiaz@gmail.com>
2024-09-26 11:34:42 -0500

    primer commit

---
+++ src/main/java/com/diver/clientapp/Model/Message.java
@@ -0,0 +1,21 @@
+package com.diver.clientapp.Model;
+
+public class Message {
+    private String text;
+
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public Message() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
